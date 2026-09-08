511cf49b85c04c2e22d0547bf6f58e2bb4523929
salimb00 <salimjama2c@gmail.com>
2026-09-08 15:28:50 +0200

    Add task model and GET/POST endpoints

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using TaskManager.Api.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -14,4 +15,40 @@ if (app.Environment.IsDevelopment())
 
 app.MapGet("/", () => "Task Manager API is running");
 
+var tasks = new List<TodoTask>
+{
+    new TodoTask
+    {
+        Id = 1,
+        Title = "Learn ASP.NET Core",
+        Description = "Build the first Task Manager API endpoint",
+        IsCompleted = false
+    },
+
+    new TodoTask
+    {
+        Id = 2,
+        Title = "Update GitHub",
+        Description = "Push the latest project changes",
+        IsCompleted = true
+    }
+};
+
+app.MapGet("/tasks", () => tasks);
+app.MapGet("/tasks/{id}", (int id) =>
+{
+    var task = tasks.FirstOrDefault(t => t.Id == id);
+
+    return task is not null
+        ? Results.Ok(task)
+        : Results.NotFound();
+});
+app.MapPost("/tasks", (TodoTask newTask) =>
+{
+    newTask.Id = tasks.Count + 1;
+
+    tasks.Add(newTask);
+
+    return Results.Created($"/tasks/{newTask.Id}", newTask);
+});
 app.Run();
